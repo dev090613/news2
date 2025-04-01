fffe66ba3f0b47c9562b93728e7092e2e36bcea7
kokoro <128305501+dev090613@users.noreply.github.com>
2025-04-01 14:25:12 +0900

    user authentication

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.urls import path, reverse
 from django.test import TestCase
 
 # Create your tests here.
@@ -31,6 +32,39 @@ class UsersManagesTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superuser)
+
+
+class SignUpTests(TestCase):
+    def test_url_exists_on_correct_location(self):
+        response = self.client.get("/accounts/signup/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse("signup"), {
+                "username ": "testuser",
+                "email": "test@example.com",
+                "password1": "testpass1234",
+                "password2 ": "testpass1234",
+            }
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(get_user_model().objects.all().count(),
+                         1)
+        self.assertEqual(
+            get_user_model().objects.all()[0].username, 
+            "testuser"
+        )
+        self.assertEqual(
+            get_user_model().objects.all()[0].email,
+            "test@example.com"
+        )
 
         
 
